cf08db952e5d90526d71f09e8f2b5c77989c9ee2
Amelia Walsh <ameliatwalsh@gmail.com>
2023-02-23 10:05:42 -0800

    update to project-dash js

--- public/js/project-dash.helpers.js
+++ public/js/project-dash.helpers.js
@@ -2,69 +2,21 @@
 const editBtn = document.getElementById('editProjectBtn')
 editBtn.addEventListener("click", (event) => {
     
-    toggleEditDisplay(event)
+    editDisplayOn()
     
 });
 
-function toggleEditDisplay(event){
-
-    const edit_ctrl = event.target.dataset.edit
-    console.log(edit_ctrl)
-
-    const project_dash = document.getElementById("ProjectDash")
-    const project_dash_edit = document.getElementById("ProjectDashEdit")
-
-    //if false, toggle to true
-    if(edit_ctrl == "false"){
-        editDisplayOn(event, project_dash, project_dash_edit)
-        
-    } else {
-        editDisplayOff(event, project_dash, project_dash_edit)
-    }
-
-    return
-}
-
-
-function editDisplayOn(event, project_dash, project_dash_edit){
-    console.log("in edit display on")
-    //change to regular dash
-    project_dash.style = "display: none;"
-    project_dash_edit.style = ""
-    
-    //change button to done
-    editBtn.innerText = "Done"
-    
-    event.target.dataset.edit = "true"
-
-    return
-
-}
-
-function editDisplayOff(event, project_dash, project_dash_edit){
-
-    console.log("in else clause")
-    //change to regular dash
-    project_dash_edit.style = "display: none;"
-    project_dash.style = ""
-
-    //change button to done
-    editBtn.innerText = "Edit"
-
-    event.target.dataset.edit = "false"
-
-    return
-
-}
-
-//Submit Project Event
+//update project button ("done") click event
 const updBtn = document.getElementById('updateProjectBtn')
 updBtn.addEventListener("click", (event) => {
+
+
   const project_id = event.target.dataset.id
-  console.log(typeof project_id)
+
   //package data for request
   const project_values = ['ProjectName', 'ProjectType', 'ProjectDescription', 'ProjectImage']
   const project_data = generateDataObj(project_values)
+
 
   //data validation
   if(!validateFormData(project_data)){
@@ -75,18 +27,43 @@ updBtn.addEventListener("click", (event) => {
 
 });
 
-//listens for user input, if user adds input hide error message
-const projectNameInput = document.getElementById('ProjectName')
-projectNameInput.addEventListener("input", (event) => {
-  hideInputError(event.target)
 
-});
+function editDisplayOn(){
 
-const projectDescInput = document.getElementById('ProjectDescription')
-projectDescInput.addEventListener("input", (event) => {
-  hideInputError(event.target)
+    const project_dash = document.getElementById("ProjectDash")
+    const project_dash_edit = document.getElementById("ProjectDashEdit")
 
-});
+    //hide edit button
+    editBtn.style = "display: none;"
+
+    //show done button
+    document.getElementById("updateProjectBtn").style = ""
+
+    //change to regular dash
+    project_dash.style = "display: none;"
+    project_dash_edit.style = ""
+    
+
+    return
+}
+
+// function editDisplayOff(){
+
+//     console.log("in else clause")
+//     //change to regular dash
+//     project_dash_edit.style = "display: none;"
+//     project_dash.style = ""
+
+//     //change button to done
+//     editBtn.innerText = "Edit"
+
+//     event.target.dataset.edit = "false"
+
+//     return
+
+// }
+
+
 
 //generate js object with values from the form
 function generateDataObj(values){
@@ -97,6 +74,9 @@ function generateDataObj(values){
 
   return project
 }
+
+//---------INPUT VALIDATION-------
+
 
 //check for empty values in object properties
 function validateFormData(data){
@@ -123,6 +103,24 @@ function hideInputError(property){
   err_message.style.display = 'none'
 }
 
+//listens for user input, if user adds input hide error message
+const projectNameInput = document.getElementById('ProjectName')
+projectNameInput.addEventListener("input", (event) => {
+  hideInputError(event.target)
+
+});
+
+//listens for user input, if user adds input hide error message
+const projectDescInput = document.getElementById('ProjectDescription')
+projectDescInput.addEventListener("input", (event) => {
+  hideInputError(event.target)
+
+});
+
+//--------- END----------
+
+//------SENT UPDATE REQUEST------
+
 //send http post request with data to the server
 function sendPost(id, data){
   $.ajax({
@@ -131,7 +129,7 @@ function sendPost(id, data){
     data: JSON.stringify(data),
     contentType: "application/json; charset=utf-8", 
     success: function() {
-        window.location.reload();
+        window.location.reload(true)
     }
   });
 }
